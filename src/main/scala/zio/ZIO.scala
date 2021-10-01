2e3b81b72d5562a7d51798ba8d52702d0c8f937e
a.khakimov <a.khakimov@tinkoff.ru>
2021-10-02 03:02:31 +0500

    3

--- src/main/scala/zio/ZIO.scala
+++ src/main/scala/zio/ZIO.scala
@@ -1,12 +1,14 @@
 package zio
 
 
-trait ZIO[+A] {
+sealed trait ZIO[+A] {
   def run(callback: A => Unit): Unit
 }
 
 object ZIO {
-  def succeedNow[A](value: A): ZIO[A] = new ZIO[A] {
+  def succeedNow[A](value: A): ZIO[A] = ZIO.Succeed(value)
+
+  case class Succeed[A](value: A) extends ZIO[A] {
     override def run(callback: A => Unit): Unit =
       callback(value)
   }
